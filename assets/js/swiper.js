84b36feefe637f78d19630160bb7f57b5753f75c
ri4y <maksimov.1913@list.ru>
2025-01-28 13:22:49 +0300

    upd

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper', {
+const swiperPlace = new Swiper('.swiper-place', {
     // Optional parameters
     spaceBetween: 20,
     enabled: true,
@@ -39,34 +39,63 @@ const swiper = new Swiper('.swiper', {
         1280: {
             allowTouchMove: true,
             slidesPerView: 3,
-            grid: {
-                rows: 2,
-                fill: "row",
-            }
-        },
-        1921: {
-            allowTouchMove: true,
-            slidesPerView: 4,
-            grid: {
-                rows: 2,
-                fill: "row",
-            }
         },
     },
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
+
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-place-btn--next',
+      prevEl: '.swiper-place-btn--prev',
     },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
+});
+
+const swiperSpeaker = new Swiper('.swiper-speaker', {
+    // Optional parameters
+    spaceBetween: 20,
+    enabled: true,
+
+    breakpoints: {
+        320: {
+            allowTouchMove: true,
+            slidesPerView: 1,
+            grid: {
+                rows: 1,
+                fill: "row",
+            }
+        },
+        480: {
+            allowTouchMove: true,
+            slidesPerView: 2,
+            grid: {
+                rows: 1,
+                fill: "row",
+            }
+        },
+        768: {
+            allowTouchMove: true,
+            slidesPerView: 3,
+            grid: {
+                rows: 1,
+                fill: "row",
+            }
+        },
+        1024: {
+            allowTouchMove: true,
+            slidesPerView: 2,
+            grid: {
+                rows: 2,
+                fill: "row",
+            }
+        },
+        1280: {
+            allowTouchMove: true,
+            slidesPerView: 3,
+        },
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-place-btn--next',
+      prevEl: '.swiper-place-btn--prev',
     },
 });
